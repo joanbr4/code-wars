288bd2ea18f26f2f34cd7c878c2d55bf0b810fa9
thejbordo <joan.bordonaba@gmail.com>
2025-01-28 21:12:03 +0100

    fix exercise, and READMe

---
+++ README.md
@@ -0,0 +1,22 @@
+# CodeWars Challenges: Visit Here
+dashboard
+
+A repository filled with codewars challenges from elementary to advanced
+
+## How It's Made:
+Teach used: Java
+
+Every day, I try to find some time to solve an exercise!
+
+## Lessons Learned:
+The Biggest Lesson I learned in tackling this endless supply of questions is understanding that there is a long way to go. Not being able to answer a question is okay, as long as you take the initiative to improve.
+
+## Where are exercise:
+
+Check in `/src` folder, and be ready to run every exercise
+
+## Specification to know
+
+In Code Wars, there 2 Rankings: 
+ - 8 Kyu to 1 Kyu ( Degree)
+ - 1 Dan to 8 Dan (Master Level)
--- src/ArrayElementParity.java
+++ src/ArrayElementParity.java
@@ -1,4 +1,18 @@
 import java.util.*;
+/*
+LEVEL: 7kyu
+In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer.
+
+example
+input: int[]
+output: int
+Examples:
+
+int[] intArray = {1,-1, -4, -3, 5, -5, 3}; //-4
+int[] intArray = {-110,110,-38,-38,-62,62,-38,-38,-38}; //-38
+int[] intArray = {-9,-105,-9,-9,-9,-9,105}; //-9
+
+* * */
 
 public class ArrayElementParity {
     private int FindNumber(int[] arr){
@@ -24,10 +38,7 @@ public class ArrayElementParity {
         return numberOnlyPosNeg;
     }
     public static void main(String[] args) {
-//        int[] intArray = {1,-1, -4, -3, 5, -5, 3}; //-4
-//        int[] intArray = {-110,110,-38,-38,-62,62,-38,-38,-38}; //38
-        int[] intArray = {-9,-105,-9,-9,-9,-9,105}; //38
-
+        int[] intArray = {};
         int result = new ArrayElementParity().FindNumber(intArray);
         System.out.println("Special number  is " + result);
     }
